41ff64cfedd8d90e19e667dc9cabae7d7b4f54a8
mafgit <abdskinny@gmail.com>
2020-09-13 22:51:29 -0700

    ...

--- app.js
+++ app.js
@@ -25,12 +25,12 @@ mongoose
 
 app.use(require('./routes/routes'))
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'))
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-  })
-}
+// if (process.env.NODE_ENV === 'production') {
+app.use(express.static('client/build'))
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+})
+// }
 
 const PORT = process.env.PORT || 5000
 

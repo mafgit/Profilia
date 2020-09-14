e093c620ba497ffc30e8e523f96be2fad50065df
mafgit <abdskinny@gmail.com>
2020-09-14 10:03:45 -0700

    ...

--- middlewares/verifyToken.js
+++ middlewares/verifyToken.js
@@ -5,17 +5,12 @@ const verifyToken = (req, res, next) => {
   let token = req.headers['authorization']
   if (token && typeof token !== undefined) {
     token = token.split(' ')[1]
-    jwt
-      .verify(token, process.env.JWT_SECRET, (err, decoded) => {
-        console.log(err, decoded)
-        if (!decoded) return res.json({ error: 'Unauthorized' })
-        req.userEmail = decoded.email
-        req.userId = ObjectId(decoded._id)
-        next()
-      })
-      .catch((err) => {
-        console.log(err)
-      })
+    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+      if (!decoded) return res.json({ error: 'Unauthorized' })
+      req.userEmail = decoded.email
+      req.userId = ObjectId(decoded._id)
+      next()
+    })
   } else {
     return res.json({ error: 'Unauthorized' })
   }
